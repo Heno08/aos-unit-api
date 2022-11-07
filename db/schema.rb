0a1c63174689247e7f704a1aaba909672a8e9f1b
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-11-07 12:13:52 +0100

    generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_102536) do
+ActiveRecord::Schema.define(version: 2022_11_07_104426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 2022_11_07_102536) do
     t.string "name"
     t.integer "unitsize"
     t.integer "wounds"
-    t.integer "save"
     t.integer "bravery"
     t.string "img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "unitsave"
   end
 
   create_table "weapons", force: :cascade do |t|
