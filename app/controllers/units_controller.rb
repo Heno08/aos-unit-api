3f812b73db5d9d830b5454780dcd21a1ac35f99e
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-11-10 16:52:24 +0100

    functions added

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,4 +3,20 @@ class UnitsController < ApplicationController
     units = Unit.all
     render json: units, include: 'weapons'
   end
+
+  def new
+    @unit = Unit.new
+  end
+
+  def create
+    @unit = Unit.new(params[unit_params])
+    @unit.save
+    redirect_to units_path
+  end
+
+  private
+
+  def unit_params
+    params.require(:unit).permit(:name, :unitsize, :wounds, :bravery, :unitsave)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :units, only: [:index]
+  resources :units, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
